41c75373ae90b7007c4cbc0ae61bf4f865162841
supratim531 <91423964+supratim531@users.noreply.github.com>
2023-07-31 16:22:31 +0530

    supratim531: Refined and removed the ORM & Sequelize stuffs

--- router/productRouter.js
+++
@@ -1,7 +0,0 @@
-const productController = require('../controller/productController');
-const router = require('express').Router();
-
-router.get('/', productController.getAllProducts);
-router.post('/', productController.addProduct);
-
-module.exports = router;
